4c5ab9d3bd026c878d7a244cd054614dba2946da
Gerald Maqellara <109540768+gerald-coding@users.noreply.github.com>
2022-08-25 00:56:10 +0200

    Completed CRUD for car listings

--- car_details/urls.py
+++ car_details/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('car_details/<int:id>/', views.car_details, name="car_details"),
-    path('car_details/<int:id>', views.review, name="review"),
+    path('car_details/review/<int:id>/', views.review, name="review"),
     path('car_details/update_review/<int:id>/', views.update_review, name="update_review"),
     path('car_details/delete_review/<int:id>/', views.delete_review, name="delete_review"),
 ]
--- car_details/views.py
+++ car_details/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from car_models.models import Car, Image, Review, Booking
 from .forms import ReviewForm
@@ -10,10 +10,11 @@ from .forms import ReviewForm
 def car_details(request, id):
 
     car = Car.objects.get(id=id)
+    user = request.user
     images = Image.objects.filter(car=car)
     reviews = Review.objects.filter(car_id=car.id)
 
-    context = {'model': car, 'reviews': reviews, 'images': images}
+    context = {'model': car, 'reviews': reviews, 'images': images, 'user': user}
 
     return render(request, 'car_details/car_details.html', context)
 
@@ -23,28 +24,32 @@ def review(request, id):
 
     user = request.user
     car = Car.objects.get(id=id)
+    form = ReviewForm()
 
     if request.method == 'POST':
-        feedback = request.POST.get('feedback')
-        score = request.POST.get('score')
-        if feedback != '' and feedback is not None and score != '' and score is not None:
-            review = Review.objects.create(
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            form = Review.objects.create(
                 user=user,
                 car=car,
-                feedback=feedback,
-                score=score,
+                feedback=data['feedback'],
+                score=data['score']
             )
-            review.save()
-            return redirect('car_details')
+            form.save()
 
-    # context = {'form': form}
-    return render(request, 'car_details/car_details.html')
+        return redirect('car_details')
+
+    context = {'form': form}
+
+    return render(request, 'car_details/review_form.html', context)
 
 
 @login_required(login_url='login')
 def update_review(request, id):
 
     review = Review.objects.get(id=id)
+    user = request.user
     form = ReviewForm(instance=review)
 
     if request.user != review.user:
@@ -54,22 +59,24 @@ def update_review(request, id):
         form = ReviewForm(request.POST, instance=review)
         if form.is_valid():
             form.save()
-            return redirect('profile')
 
-    context = {'form': form}
-    return render(request, 'account/car_form.html', context)
+            return redirect('car')
+
+    context = {'form': form, 'review': review}
+    return render(request, 'car_details/review_form.html', context)
 
 
 @login_required(login_url='login')
 def delete_review(request, id):
 
     review = Review.objects.get(id=id)
+    user = request.user
 
     if request.user != review.user:
         return HttpResponse('You are not allowed to delete this car')
 
     if request.method == 'POST':
         review.delete()
-        return redirect('profile')
-    return render(request, 'account/delete_car_form.html', {'obj': review})
+        return redirect('car')
+    return render(request, 'car_details/review_form.html', {'obj': review})
 # CRUD views for Car listings end here
